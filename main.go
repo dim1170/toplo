d09207bb133386ac1e86dda1c52f951fbfe6c523
dimitr <dimrrr@yahoo.com>
2025-04-06 17:58:32 +0300

    code imp

--- main.go
+++ main.go
@@ -13,75 +13,81 @@ import (
 
 var apiToplo string = "https://api.toplo.bg/api"
 
-func sess() string {
-	user := os.Getenv("USER_TOPLO") // from ENV (tested with linux env) : username from toplo registration
-	pass := os.Getenv("PASS_TOPLO") // from ENV (tested with linux env) : password from toplo registration
+func sess() *http.Request {
 
-	postDataM := map[string]interface{}{
-		"Email":    user,
-		"Password": pass,
+	postDataM := map[string]any{
+		"Email":    os.Getenv("USER_TOPLO"), // from ENV (tested with linux env) : username from toplo registration
+		"Password": os.Getenv("PASS_TOPLO"), // from ENV (tested with linux env) : password from toplo registration
 	}
+
 	postData, err := json.Marshal(postDataM)
 	if err != nil {
 		log.Fatal("cannot create json ", err)
 	}
-	client := &http.Client{}
+
 	req, err := http.NewRequest("POST", apiToplo+"/auth/login", bytes.NewBuffer(postData))
 	if err != nil {
 		log.Fatal("cannot create post request:", err)
 	}
-
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("there is no response from server: ", err)
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("cannot read body: ", err)
-	}
-	var cont map[string]any
-	json.Unmarshal(body, &cont)
-	sess := fmt.Sprintf("%s", cont["token"])
-	return sess
+	return req
 }
 
-func jOut(sess string) {
-	client := &http.Client{}
+func jOut(sess string) (req *http.Request) {
+
 	req, err := http.NewRequest("GET", apiToplo+"/Stations/GetStationStandartView", nil)
 	if err != nil {
 		log.Fatal("cannot create get request: ", err)
 	}
 	var bearer = "Bearer " + sess
 	req.Header.Add("Authorization", bearer)
+	return req
+}
+
+func httpz(valS string, token string) (body []byte) {
+	client := &http.Client{}
+
+	var req *http.Request
+	if valS == "token" {
+		req = sess()
+	} else if valS == "jVal" {
+		req = jOut(token)
+	}
+	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal("response from server is empty: ", err)
 	}
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
+	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal("cannot read read body: ", err)
 	}
-	var cont map[string]any
-	json.Unmarshal(body, &cont)
 
-	pref := cont["$values"].([]interface{})[0].(map[string]interface{})
-
-	t := time.Now()
-	fmt.Printf("Адрес : %s\n", pref["name"])
-	fmt.Printf("Дата и час на замерване от топлофикация : %02d:%02d:%02d %02d-%02d-%d\n", t.Hour(), t.Minute(), t.Second(), t.Day(), t.Month(), t.Year())
-	fmt.Printf("Температура околна среда(извън блока) : %v\n", pref["outsideTemperature"])
-	fmt.Printf("Температура топла вода на входа на блока : %v\n", pref["heatmeterTEmitting"])
-	fmt.Printf("Температура топла вода за парно : %v\n", pref["heatingMeasuredTemperature"])
-	fmt.Printf("Температура топла вода за ВиК : %v\n", pref["domesticHotWaterMeasuredTemperature"])
+	return body
 
 }
 
 func main() {
-	aSession := sess()
-	jOut(aSession)
+
+	bodyT := httpz("token", "")
+	var cont map[string]any
+	json.Unmarshal(bodyT, &cont)
+
+	token := fmt.Sprintf("%s", cont["token"])
+
+	jValues := httpz("jVal", token)
+
+	var jValue map[string]any
+	json.Unmarshal(jValues, &jValue)
+	wValue := jValue["$values"].([]interface{})[0].(map[string]interface{})
+
+	t := time.Now()
+	fmt.Printf("Адрес : %s\n", wValue["name"])
+	fmt.Printf("Дата и час на замерване от топлофикация : %02d:%02d:%02d %02d-%02d-%d\n", t.Hour(), t.Minute(), t.Second(), t.Day(), t.Month(), t.Year())
+	fmt.Printf("Температура околна среда(извън блока) : %v\n", wValue["outsideTemperature"])
+	fmt.Printf("Температура топла вода на входа на блока : %v\n", wValue["heatmeterTEmitting"])
+	fmt.Printf("Температура топла вода за парно : %v\n", wValue["heatingMeasuredTemperature"])
+	fmt.Printf("Температура топла вода за ВиК : %v\n", wValue["domesticHotWaterMeasuredTemperature"])
+
 }
